6e7e58ebfae41d0772021d56a4c1526ee1a3642c
Codebreakerrr2 <144541673+Codebreakerrr2@users.noreply.github.com>
2024-02-21 03:46:08 +0100

    halbwegs fertig

---
+++ src/test/java/Graphs/GraphenTest.java
@@ -0,0 +1,52 @@
+package Graphs;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GraphenTest {
+
+    @Test
+    void anfangsKnotenVon() {
+    }
+
+    @Test
+    void endKnotenVon() {
+    }
+
+    @Test
+    void knotenHinzufügen() {
+    }
+
+    @Test
+    void kantenHinzufügen() {
+    }
+
+    @Test
+    void getAnzahlKnoten() {
+    }
+
+    @Test
+    void getAnzahlKanten() {
+    }
+
+    @Test
+    void getInnenGrad() {
+    }
+
+    @Test
+    void getAußenGrad() {
+    }
+
+    @Test
+    void getKnotenGrad() {
+    }
+
+    @Test
+    void getNachfolgerKnoten() {
+    }
+
+    @Test
+    void getVorGängerKnoten() {
+    }
+}
